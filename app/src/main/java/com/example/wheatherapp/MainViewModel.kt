cefa7a0766c195bd29a119a3107aceaecae847b7
sergey_gap <1ggserongg1@gmail.com>
2022-11-17 00:58:13 +0300

    add list_item.xml

--- app/src/main/java/com/example/wheatherapp/MainViewModel.kt
+++ app/src/main/java/com/example/wheatherapp/MainViewModel.kt
@@ -2,10 +2,11 @@ package com.example.wheatherapp
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.wheatherapp.adapters.DayItem
 
 class MainViewModel: ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<DayItem>()
+    val liveDataList = MutableLiveData<List<DayItem>>()
 
 
 }
---
+++ app/src/main/java/com/example/wheatherapp/adapters/WeatherAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.wheatherapp.adapters
+
+class WeatherAdapter {
+}
